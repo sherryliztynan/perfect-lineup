e54f6e71a2fd6e5ee0d2d6626fa81a01fc766908
Sherron <sherron@Sherrons-MacBook-Air.local>
2021-08-01 22:39:05 -0400

    used a removeUnique function found from stack ovrflow to push duplicate teams to new array and then checked for length

--- index.js
+++ index.js
@@ -19,7 +19,41 @@ function validateSalary(lineup) {
 }
 
 
+// team.Id <2 same team
+function validateTeams(lineup) {
+  const teamArray = lineup.map((team) => team.teamId)
 
+  function removeUnique(arr) {
+    var newArr = []
+
+    for (var i = 0; i < arr.length; i++) {
+      var count = 1
+
+      for (var j = i + 1; j < arr.length; j++) {
+        if (arr[j] === arr[i]) {
+          count++
+        }
+      }
+      if (count > 2) {
+        newArr.push(arr[i])
+      }
+    }
+
+    return newArr
+  }
+  let correctTeams = removeUnique(teamArray)
+
+  if (correctTeams.length > 0) {
+    correctTeams = false
+
+    return correctTeams
+  }
+  else {
+    correctTeams = true
+  }
+
+  return correctTeams
+}
 
 
 
